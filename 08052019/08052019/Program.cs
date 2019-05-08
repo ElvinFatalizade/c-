535c9eb5489a572e1444eb2a65d43be1a3a69c0a
Elvin Fatalizade <elvinff@code.edu.az>
2019-05-08 16:03:17 +0400

    C#

---
+++ 08052019/08052019/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08052019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            // int a = 10;
+            //int b = 17;
+            // string city = "Paris";
+            // string capital = "Baku";
+
+            // a += 12;
+            // a -= 5;
+
+
+            // if (city == capital)
+            // {
+            //         Console.WriteLine("That is right");
+
+            //  }
+            //  else
+            // {
+            //        Console.WriteLine("That is no right");
+            // }
+            //   Console.ReadLine();
+
+            //int[] ages={ 13,34,56,87};
+
+            //  foreach(int item in ages)
+            //  {
+            //     Console.WriteLine(item);
+
+            //      Console.ReadLine();
+
+            //    }
+
+
+            student stu1 = new student();
+
+            stu1.Name = "Behruz";
+            stu1.Surname ="Fatalizade";
+           
+
+            car car1 = new car();
+            Console.WriteLine(car1.currentfuel);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ 08052019/08052019/student.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08052019
+{
+    class student
+    {
+        private string _Name { get; set; }
+
+        public string Surname { get; set; }
+
+        private int Age { get; set; }
+
+       public String Name {
+            get
+            {
+                return this._Name;
+            }
+            set
+            {
+                this._Name = value;
+            }
+    }          
+
+    }
+
+    class car
+    {
+        private double _fuelusage;
+        private double _fullcapacity;
+        private double _currentfuel =90;
+
+
+        public double currentfuel
+        {
+            get
+            {
+                return _currentfuel;
+            }
+            
+        }
+
+        public void AddFuel(double litr)
+        {
+            if (_currentfuel + litr >= this._fullcapacity)
+            {
+                return;
+            }
+            this._currentfuel += litr;
+        }
+        private double _globalkm;
+
+        public double globalkm
+        {
+            get
+            {
+                return this._globalkm;
+            }
+        }
+        private double _localkm;
+
+        public double localkm
+        {
+            get
+            {
+                return _localkm;
+            }
+        }
+        public void Resetlocal()
+        {
+            this._localkm = 0;
+        }
+
+    }
+}
